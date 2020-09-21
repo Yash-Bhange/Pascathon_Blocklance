7d8f5bae4c642c8385a9bfa0739f2b47349be5f6
yash bhange <yashbhange888@gmail.com>
2020-09-21 14:21:28 +0530

    add other function in smart contract

--- smartcontract/contracts/IdentityManagemnet.sol
+++ smartcontract/contracts/IdentityManagemnet.sol
@@ -6,7 +6,7 @@ contract IdentityManagement
 
     address ContractOwner;
     
-    constructor()public {
+    constructor() public {
         ContractOwner = msg.sender;
     }
 
@@ -23,12 +23,29 @@ contract IdentityManagement
 		bytes DL_Hash;
 		string DL_Address;
     }
-
+    struct DLRequest{
+		string RequestedBy;
+		uint DL_No;
+		uint DL_Name;
+		uint DL_DOB;
+		uint DL_Hash;
+		uint DL_Address;
+		uint DL_OverAll_Status;
+    }
+     /*
+            ApprovalStatus
+        -------------
+        0 --  default status
+        1 --  Requested
+        2 --  Approved
+        3 --  Rejected
+    */
    
   
 
     mapping(address => UserInfo[]) UserMap;
 	mapping(address => UserDL[]) UserDLMap;
+    mapping(address => DLRequest[]) DLRequestMap;
 	
 	
     function AddUser(address UserAddress,string memory FullName,string memory EmailID,uint MobileNo) public
@@ -40,6 +57,16 @@ contract IdentityManagement
     {
         UserDLMap[UserAddress].push(UserDL(DL_No, DL_Name, DL_DOB, DL_Hash, DL_Address));
     }
+
+     function AddDLRequest(address UserAddress,string memory RequestedBy, uint DL_No, uint DL_Name, uint DL_DOB, uint DL_Hash, uint DL_Address, uint DL_OverAll_Status)public
+    {
+        DLRequestMap[UserAddress].push(DLRequest(RequestedBy, DL_No, DL_Name, DL_DOB, DL_Hash, DL_Address, DL_OverAll_Status));
+    }
+     function ViewDLRequestLength(address UserAddress) public returns(uint)
+    {
+        return DLRequestMap[UserAddress].length;
+    }
+
 
 
 
